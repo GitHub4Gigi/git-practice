93f48cd300815b3cef8b4af2ec1817157c051576
Joanna Dudley <jfdudleybmc@gmail.com>
2022-03-25 16:45:33 -0400

    breaking function d

--- function_d.py
+++ function_d.py
@@ -2,6 +2,10 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
+    sum = 0
+    for num in numbers:
+        sum += num
+    
     return numbers.max()
 
 
