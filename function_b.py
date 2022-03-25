58fe7e249d10b1adb4d811d880c36cfec45b53ed
Gricelda <g_maycock@outlook.com>
2022-03-25 13:40:07 -0700

    Gricelda added function b

--- function_b.py
+++ function_b.py
@@ -6,8 +6,20 @@ def silly_sum():
         summing as we go until either
         the user enters 0, or
         the sum reaches or exceeds 1000
+    
     """
-    pass
+    num = int(input('Please enter a number => '))
+    sum = 0
+
+    while num != 0:
+        sum += num
+        if sum >= 1000:
+            break
+
+        num = int(input('Please enter a number => '))
+
+    return sum
+
 
 
 if __name__ == "__main__":
